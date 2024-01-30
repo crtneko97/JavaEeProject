0a1c8c13bb025a72e86d56f5e66e1e2ded8f2fb6
crtneko97 <simon_kern@hotmail.com>
2024-01-30 14:34:34 +0100

    updates i will change later have to fix the postMapping thingy creates new user

--- src/main/java/com/example/enterprisecourse/controllers/UserController.java
+++ src/main/java/com/example/enterprisecourse/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.example.enterprisecourse.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -35,6 +37,28 @@ public class UserController {
     }
     
     
+    
+
+    //
+    @GetMapping("/showUpdateUser/{id}")
+    public String updateUser(@PathVariable(value = "id") long id, Model model) {
+    	
+    	UserEntity user = userService.getUserById(id);
+    	
+    	model.addAttribute("user", user);
+    
+    	
+    	return "updateUser";
+    }
+    
+    // Lägg till auth senare. yahya <3
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") UserEntity user ){
+    	
+    	this.userService.saveUser(user);
+    	
+    	return "redirect:/adminpage";
+    }
     
     @GetMapping("/deleteUser/{id}")
     public String deleteUser(@PathVariable(value = "id") long id) {
--- src/main/java/com/example/enterprisecourse/services/UserService.java
+++ src/main/java/com/example/enterprisecourse/services/UserService.java
@@ -14,5 +14,6 @@ public interface UserService {
 	
 	void saveUser(UserEntity user);
 	
+	
 
 }
